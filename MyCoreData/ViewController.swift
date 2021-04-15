1f55840df980c1c2659d2469b48a000677cf0525
b10715027 <b10715027@gapps>
2021-04-15 12:10:20 +0800

    Initial Commit

---
+++ MyCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoreData
+//
+//  Created by mac10 on 2021/4/15.
+//  Copyright © 2021 mac10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
